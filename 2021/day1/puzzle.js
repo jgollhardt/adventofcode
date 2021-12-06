4d5c923ed9c25c1d9ae2dfb2822baae3eee3db5c
Jared Gollhardt <jared@startplayinggames.com>
2021-12-05 20:54:37 -0800

    day1 solutions

--- 2021/day1/puzzle.js
+++ 2021/day1/puzzle.js
@@ -2,18 +2,35 @@ import _ from 'lodash';
 import fs from 'fs';
 import { fetchInput } from '../../utils/fetch.js';
 
-//
+// 1557
 const puzzle1 = (lines) => {
-  return lines;
+  let result = 0;
+  for (let i = 1; i < lines.length; i++) {
+    if (lines[i] > lines[i - 1]) {
+      result += 1;
+    }
+  }
+  return result;
 };
 
-//
-const puzzle2 = (lines) => {};
+// 1608
+const puzzle2 = (lines) => {
+  let result = 0;
+  for (let i = 1; i < lines.length; i++) {
+    if (lines[i] > lines[i - 3]) {
+      result += 1;
+    }
+  }
+  return result;
+};
 
 await fetchInput();
 
 // const data = fs.readFileSync('test_input1.txt', 'utf-8');
 const data = fs.readFileSync('input.txt', 'utf-8');
-const lines = data.trim().split('\n');
+const lines = data
+  .trim()
+  .split('\n')
+  .map((line) => parseInt(line));
 console.log(puzzle1(lines));
 console.log(puzzle2(lines));

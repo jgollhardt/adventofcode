e3de395885338d7d0a72e66db4fd473dbb95c380
Jared Gollhardt <jared@startplayinggames.com>
2021-12-14 22:51:51 -0800

    didn't need to store whole path

--- 2021/day15/puzzle.js
+++ 2021/day15/puzzle.js
@@ -4,16 +4,15 @@ import { fetchInput } from '../../utils/fetch.js';
 
 // 487
 const puzzle1 = (lines) => {
-  let paths = [{ path: [[0, 0]], risk: 0 }];
+  let paths = [{ pos: [0, 0], risk: 0 }];
   const seen = {};
   while (paths.length > 0) {
-    const { path, risk } = paths.shift();
-    const lastPos = path[path.length - 1];
-    const existingRisk = seen[lastPos.toString()];
+    const { pos, risk } = paths.shift();
+    const existingRisk = seen[pos.toString()];
     if (existingRisk !== undefined) {
       continue;
     } else {
-      seen[lastPos.toString()] = risk;
+      seen[pos.toString()] = risk;
     }
 
     const dirs = [
@@ -23,12 +22,12 @@ const puzzle1 = (lines) => {
       [1, 0],
     ];
     dirs.forEach((dir) => {
-      const newX = lastPos[0] + dir[0];
-      const newY = lastPos[1] + dir[1];
+      const newX = pos[0] + dir[0];
+      const newY = pos[1] + dir[1];
       if (lines[newY] && lines[newY][newX] !== undefined) {
         const newRisk = risk + lines[newY][newX];
         paths.push({
-          path: [...path, [newX, newY]],
+          pos: [newX, newY],
           risk: newRisk,
         });
       }

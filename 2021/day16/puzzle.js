21de8c7df6ba4bf3d6555b85f40d01a6c14e955f
Jared Gollhardt <4246996+jgollhardt@users.noreply.github.com>
2025-09-12 00:52:27 -0700

    Default to test input

--- 2021/day16/puzzle.js
+++ 2021/day16/puzzle.js
@@ -12,8 +12,8 @@ const puzzle2 = (lines) => {};
 
 await fetchInput();
 
-// const data = fs.readFileSync('test_input1.txt', 'utf-8');
-const data = fs.readFileSync('input.txt', 'utf-8');
+const data = fs.readFileSync('test_input1.txt', 'utf-8');
+// const data = fs.readFileSync('input.txt', 'utf-8');
 const lines = data.trim().split('\n');
 console.log(puzzle1(lines));
 console.log(puzzle2(lines));

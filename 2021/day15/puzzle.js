c0500ccee368cac121e0e983f291d3a7e0cec775
Jared Gollhardt <4246996+jgollhardt@users.noreply.github.com>
2025-09-12 00:52:27 -0700

    code reuse

--- 2021/day15/puzzle.js
+++ 2021/day15/puzzle.js
@@ -60,42 +60,7 @@ const puzzle2 = (oldLines) => {
     });
   }
 
-  let paths = [{ path: [[0, 0]], risk: 0 }];
-  const seen = {};
-  while (paths.length > 0) {
-    const { path, risk } = paths.shift();
-    const lastPos = path[path.length - 1];
-    const existingRisk = seen[lastPos.toString()];
-    if (existingRisk !== undefined) {
-      continue;
-    } else {
-      seen[lastPos.toString()] = risk;
-    }
-
-    const dirs = [
-      [0, 1],
-      [0, -1],
-      [-1, 0],
-      [1, 0],
-    ];
-    dirs.forEach((dir) => {
-      const newX = lastPos[0] + dir[0];
-      const newY = lastPos[1] + dir[1];
-      if (lines[newY] && lines[newY][newX] !== undefined) {
-        const newRisk = risk + lines[newY][newX];
-        paths.push({
-          path: [...path, [newX, newY]],
-          risk: newRisk,
-        });
-      }
-    });
-
-    // sort paths with lowest risk first
-    paths = paths.sort((pathA, pathB) => pathA.risk - pathB.risk);
-  }
-
-  // not 46462
-  return seen[[lines.length - 1, lines[0].length - 1].toString()];
+  return puzzle1(lines);
 };
 
 await fetchInput();

e908e1c1ecf3e4ee51c839f3625d49eb33570562
Jared Gollhardt <4246996+jgollhardt@users.noreply.github.com>
2025-12-01 15:38:41 -0800

    Update fetch util to allow days starting with 0

--- utils/fetch.js
+++ utils/fetch.js
@@ -7,7 +7,7 @@ dotenv.config({ path: '../../.env' });
 const fetchInput = async () => {
   if (!fs.existsSync('input.txt')) {
     let [year, day] = process.cwd().split('/').slice(-2);
-    day = day.replace('day', '');
+    day = day.replace(/day0?/, '');
     const res = await fetch(
       `https://adventofcode.com/${year}/day/${day}/input`,
       {
